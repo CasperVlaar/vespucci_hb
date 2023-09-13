2b89d07b3da65c2999767ba9e341e4d22ae618c4
CasperV06 <99741484+CasperVlaar@users.noreply.github.com>
2023-09-13 14:38:34 +0200

    Import vespucci HB

---
+++ __resource.lua
@@ -0,0 +1,5 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+data_file('DLC_ITYP_REQUEST')('int_corporate.ytyp')
+data_file('DLC_ITYP_REQUEST')('v_int_38.ytyp')
+
+this_is_a_map "yes"
